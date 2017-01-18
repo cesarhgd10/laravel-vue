eb1ec919bc33c79ecac18427e7ed956ee9e7e364
Cesar Damascena <cesar.damascena@resultate.com.br>
2017-01-18 16:36:26 -0200

    v-click

--- vue.js/app.js
+++ vue.js/app.js
@@ -4,6 +4,10 @@ var app = new Vue({
   el: "#app",
   data: {
     title: "Contas a pagar",
+    menus: [
+      {id: 0, name: "Listar contas"},
+      {id: 1, name: "Criar conta"}
+    ],
     bills: [
       {date_due: '20/08/2016', name: 'Conta', value: 70.30 , done: 1},
       {date_due: '20/08/2016', name: 'Conta', value: 70.00 , done: 0},
@@ -23,6 +27,11 @@ var app = new Vue({
       }
 
       return !count ? "Nenhuma conta a pagar": "Existem " + count + " contas a serem pagas";
+    }
+  },
+  methods: {
+    showView: function(number) {
+      console.log(number);
     }
   }
 });
