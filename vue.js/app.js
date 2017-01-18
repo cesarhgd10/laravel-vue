5664fc71645cf7322b56ed27f81dea94d4abd953
Cesar Damascena <cesar.damascena@resultate.com.br>
2017-01-18 16:08:14 -0200

    Initial commit

---
+++ vue.js/app.js
@@ -0,0 +1,28 @@
+Vue.config.devtools = true;
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    title: "Contas a pagar",
+    bills: [
+      {date_due: '20/08/2016', name: 'Conta', value: 70.30 , done: 1},
+      {date_due: '20/08/2016', name: 'Conta', value: 70.00 , done: 0},
+      {date_due: '20/08/2016', name: 'Conta', value: 70.00 , done: 0},
+      {date_due: '20/08/2016', name: 'Conta', value: 70.00 , done: 0},
+      {date_due: '20/08/2016', name: 'Conta', value: 70.00 , done: 0},
+      {date_due: '20/08/2016', name: 'Conta', value: 70.00 , done: 0},
+    ]
+  },
+  computed: {
+    status: function() {
+      var count = 0;
+      for(var i in this.bills) {
+        if (!this.bills[i].done) {
+          count ++;
+        }
+      }
+
+      return !count ? "Nenhuma conta a pagar": "Existem " + count + " contas a serem pagas";
+    }
+  }
+});
